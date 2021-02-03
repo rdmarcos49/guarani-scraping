3a55c05794276e930dada5c58828233828e241be
rdmarcos49 <roberto.dmarcos@hotmail.com>
2021-02-02 22:04:51 -0300

    added logic for put the plan 2011

--- index.js
+++ index.js
@@ -35,29 +35,24 @@ function theInnerTextIncludesSomeOfThisWords(innerTextOption, ...words) {
 }
 
 function getIngSistValue(options) {
-
   const CAREER_WORDS = ['ingenieria', 'ingeniería'];
   let target = null;
-
   options.forEach(option => {    
-      if (theInnerTextIncludesSomeOfThisWords(option.innerText, ...CAREER_WORDS)) {
-        target = option.value;
-      }
-    })
-
+    if (theInnerTextIncludesSomeOfThisWords(option.innerText, ...CAREER_WORDS)) {
+      target = option.value;
+    }
+  })
   return target;
-
 }
 
 function getPlan2011(options) {
   const PLAN = ['2011'];
   let target = null;
-  for (const option of options) {
-    const actualOptionWords = option.innerText;
-    if (theInnerTextIncludesSomeOfThisWords(actualOptionWords, ...PLAN)) {
+  options.forEach(option => {    
+    if (theInnerTextIncludesSomeOfThisWords(option.innerText, ...PLAN)) {
       target = option.value;
     }
-  }
+  })
   return target;
 }
 
@@ -74,37 +69,45 @@ const URL = 'https://g3w.exa.unicen.edu.ar/guarani3w/fecha_examen';
 
   await page.goto(URL, {waitUntil: 'networkidle0'});
   await page.screenshot({path: 'screenshots/initOfPage.png'})
-  await page.exposeFunction("getIngSistValue", getIngSistValue);
+  await page.exposeFunction('getIngSistValue', getIngSistValue);
+  
   const career = await page.evaluate(() => {
     const options = [...document.querySelectorAll('#formulario_filtro-carrera option')].map(option => {
       return {
-          innerText:option.innerText,
-          value:option.value
+          innerText: option.innerText,
+          value: option.value
         }
     })
-
     const ingSistValue = window.getIngSistValue(options);
     return ingSistValue;
   })
   console.log(career);
   await page.select('#formulario_filtro-carrera', career);
   await page.screenshot({path: 'screenshots/selectedCareer.png'})
-
-  /*
+  await page.exposeFunction('getPlan2011', getPlan2011);
+  
   const plan = await page.evaluate(() => {
-    const options = document.querySelectorAll('#formulario_filtro-plan option');
-    const plan2011Value = getPlan2011(options);
+    const options = [...document.querySelectorAll('#formulario_filtro-plan option')].map(option => {
+      return {
+          innerText: option.innerText,
+          value: option.value
+        }
+    })
+    const plan2011Value = window.getPlan2011(options);
     return plan2011Value;
   });
+  console.log(plan);
   await page.select('#formulario_filtro-plan', plan);
   await page.screenshot({path: 'screenshots/selecterPlan.png'})
 
-  await pageEvaluate(() => {
+  await page.evaluate(() => {
     const button = document.getElementById('boton_buscar');
     button.click();
   })
+  await page.waitForSelector('.corte')
   await page.screenshot({path: 'screenshots/searchResult.png'})
 
-  */
+  await browser.close();
+
   
 })();
