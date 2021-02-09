db4c5a501b5d3863cad508531c3c3d6334d1e719
rdmarcos49 <roberto.dmarcos@hotmail.com>
2021-02-09 11:56:28 -0300

    dynamic careers and plans

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ const URL = 'https://g3w.exa.unicen.edu.ar/guarani3w/fecha_examen';
   await page.exposeFunction('getObjetoDeInformacionDeLaMesa', getObjetoDeInformacionDeLaMesa);
   await page.exposeFunction('getInfoCompleta', getInfoCompleta);
   
-  const careerArray = await page.evaluate(() => {
+  const careers = await page.evaluate(() => {
     const options = [...document.querySelectorAll('#formulario_filtro-carrera option')].map(option => {
       return {
           name: option.innerText,
@@ -95,79 +95,87 @@ const URL = 'https://g3w.exa.unicen.edu.ar/guarani3w/fecha_examen';
     })
     return filteredOptions;
   })
-  const career = careerArray[0];
-  await page.select('#formulario_filtro-carrera', career.value);
-  await page.screenshot({path: 'screenshots/selectedCareer.png'})
-  await page.exposeFunction('getPlan2011', getPlan2011);
   
-  const planes = await page.evaluate(() => {
-    const options = [...document.querySelectorAll('#formulario_filtro-plan option')].map(option => {
-      return {
-          name: option.innerText,
-          value: option.value
-        }
-    })
-    let filteredOptions = [];
-    options.forEach(option => {
-      if (!!option.value) {
-        filteredOptions.push(option);
-      }
-    })
-    return filteredOptions;
-  });
 
 
   let informationToWrite = []
-  for (const plan of planes) {
+  for (const career of careers) {
+
     await page.goto(URL, {waitUntil: 'networkidle0'});
+    await page.waitForSelector('#formulario_filtro-carrera')
+
     await page.select('#formulario_filtro-carrera', career.value);
+    await page.screenshot({path: 'screenshots/selectedCareer.png'})
     await page.waitForTimeout(1000)
-    await page.select('#formulario_filtro-plan', plan.value);
-    await page.screenshot({path: 'screenshots/selecterPlan.png'})
-
-    await page.evaluate(() => {
-      const button = document.getElementById('boton_buscar');
-      button.click();
-    });
-    await page.waitForSelector('.corte')
-    await page.screenshot({path: 'screenshots/searchResult.png'})
-
-    const data = await page.evaluate(async () => {
-      let clusterOfSubjects = [...document.querySelectorAll('.corte')].map( async (subject) => {
-        const subjectName = subject.querySelectorAll('.span12')[0].innerText;
-        const nodeListPrincipalHeaders = subject.querySelectorAll('table thead tr')[0];
-        const principalHeaders = [...nodeListPrincipalHeaders.querySelectorAll('th')].map(header => header.innerText);
-        const nodeListVerMasHeaders = subject.querySelectorAll('table tbody .mas_info')[0];
-        const verMasHeaders = [...nodeListVerMasHeaders.querySelectorAll('table thead tr th')].map(header => header.innerText);
-        const body = [...subject.querySelector('table tbody').childNodes];
-
-        let mesas = []
-        for (let i = 0; i < body.length; i = i + 2) {
-          const principalData = [...body[i].querySelectorAll('td')].map(elem => elem.innerText);
-          
-          const verMasData = [...body[i + 1].querySelector('td table tbody tr').childNodes].map(elem => {
-            return elem.innerText.split('\t').join('').split('\n').join('');
-          });
-          
-          const splittedInformation = await Promise.all([
-            window.getObjetoDeInformacionDeLaMesa(principalHeaders, principalData),
-            window.getObjetoDeInformacionDeLaMesa(verMasHeaders, verMasData)
-          ]);
-
-          const unifiedInfo = await window.getInfoCompleta(splittedInformation[0], splittedInformation[1])
-
-          mesas.push(unifiedInfo)
+    
+    const planes = await page.evaluate(() => {
+      const options = [...document.querySelectorAll('#formulario_filtro-plan option')].map(option => {
+        return {
+            name: option.innerText,
+            value: option.value
+          }
+      })
+      let filteredOptions = [];
+      options.forEach(option => {
+        if (!!option.value) {
+          filteredOptions.push(option);
         }
-        return {materia: subjectName, mesas: mesas};
-      });
-
-      return await Promise.all(clusterOfSubjects);
+      })
+      return filteredOptions;
     });
-    informationToWrite.push({plan: plan.name, data:data});
+
+    for (const plan of planes) {
+      await page.goto(URL, {waitUntil: 'networkidle0'});
+      await page.select('#formulario_filtro-carrera', career.value);
+      await page.waitForTimeout(1000)
+      await page.select('#formulario_filtro-plan', plan.value);
+      await page.screenshot({path: 'screenshots/selecterPlan.png'})
+  
+      await page.evaluate(() => {
+        const button = document.getElementById('boton_buscar');
+        button.click();
+      });
+      await page.waitForSelector('.corte')
+      await page.screenshot({path: 'screenshots/searchResult.png'})
+  
+      const data = await page.evaluate(async () => {
+        let clusterOfSubjects = [...document.querySelectorAll('.corte')].map( async (subject) => {
+          const subjectName = subject.querySelectorAll('.span12')[0].innerText;
+          const nodeListPrincipalHeaders = subject.querySelectorAll('table thead tr')[0];
+          const principalHeaders = [...nodeListPrincipalHeaders.querySelectorAll('th')].map(header => header.innerText);
+          const nodeListVerMasHeaders = subject.querySelectorAll('table tbody .mas_info')[0];
+          const verMasHeaders = [...nodeListVerMasHeaders.querySelectorAll('table thead tr th')].map(header => header.innerText);
+          const body = [...subject.querySelector('table tbody').childNodes];
+  
+          let mesas = []
+          for (let i = 0; i < body.length; i = i + 2) {
+            const principalData = [...body[i].querySelectorAll('td')].map(elem => elem.innerText);
+            
+            const verMasData = [...body[i + 1].querySelector('td table tbody tr').childNodes].map(elem => {
+              return elem.innerText.split('\t').join('').split('\n').join('');
+            });
+            
+            const splittedInformation = await Promise.all([
+              window.getObjetoDeInformacionDeLaMesa(principalHeaders, principalData),
+              window.getObjetoDeInformacionDeLaMesa(verMasHeaders, verMasData)
+            ]);
+  
+            const unifiedInfo = await window.getInfoCompleta(splittedInformation[0], splittedInformation[1])
+  
+            mesas.push(unifiedInfo)
+          }
+          return {materia: subjectName, mesas: mesas};
+        });
+  
+        return await Promise.all(clusterOfSubjects);
+      });
+      informationToWrite.push({career: career.name, plan: plan.name, data:data});
+    }
   }
+  
 
   informationToWrite.forEach(data => {
-    fs.writeFileSync(`${resultDir}/${career.name}-${data.plan}.json`, JSON.stringify(data, null, 2));
+    fs.writeFileSync(`${resultDir}/${data.career}-${data.plan}.json`, JSON.stringify(data, null, 2));
   })
 
   await browser.close();
