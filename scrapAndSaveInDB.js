6404a83e936419ba50c374c2dc550cac059eab86
Roberto David Marcos <roberto.marcos@globant.com>
2021-04-20 10:36:44 -0300

    names changed in db files

--- scrapAndSaveInDB.js
+++ scrapAndSaveInDB.js
@@ -1,20 +1,15 @@
-
 const db = require('./src/connection')
-const fs = require('fs');
 const scraping = require('./src/Scraping/index')
 
-const resultDir = './scraping-result';
+async function updateDB()
+{
+  const fullData = await scraping.getGuaraniesData()
+  const dbConnection = await db.connect()
 
+  fullData.forEach(async (data) => {
+    const dbColl =  dbConnection.collection(data.departament)
+    await dbColl.insertOne(data)
+  })
+}
 
-
-   async function A()
-   {
-        const fullData = await scraping.getGuaraniesData()
-        const dbConnection = await db.connect()
-
-        fullData.forEach(async (data) => {
-          const dbColl =  dbConnection.collection(data.departament)
-          await dbColl.insertOne(data)
-        })
-   }
-   A()
+updateDB()
